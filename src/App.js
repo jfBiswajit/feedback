b1c6f4a147580487fbe2d9a848b03c361b5fcbe2
Biswajit Biswas <jfbiswajit@gmail.com>
2022-01-24 21:38:37 +0600

    Add: S2E5

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
 function App() {
-  return <h1>Hi there</h1>;
+  const title = 'Blog post';
+  const body = 'This is blog post body!';
+  const comments = [
+    {
+      id: 1,
+      text: 'Comment one',
+    },
+    {
+      id: 2,
+      text: 'Comment two',
+    },
+    {
+      id: 3,
+      text: 'Comment three',
+    },
+  ];
+
+  return (
+    <div>
+      <h1> {title} </h1>
+      <p> {body} </p>
+      <h3> Comments ({comments.length}) </h3>
+      <ul>
+        {comments.map((comment) => (
+          <li key={comment.id}> {comment.text} </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
