6ab4dbec8dbed5f813ae8c093ed82373b51788a9
Biswajit Biswas <jfbiswajit@gmail.com>
2022-01-28 11:06:59 +0600

    Add: S4E3

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,8 +4,20 @@ import Button from './shared/Button';
 
 function FeedbackForm() {
   const [ratText, setRatText] = useState('');
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [errMsg, setErrMsg] = useState('');
 
   const handelOnChange = (e) => {
+    if (ratText === '') {
+      setBtnDisabled(true);
+      setErrMsg(null);
+    }else if (ratText !== '' && ratText.trim().length < 9) {
+      setBtnDisabled(true);
+      setErrMsg('Text must be at least 10 charecter');
+    }else {
+      setBtnDisabled(false);
+      setErrMsg(null);
+    }
     setRatText(e.target.value);
   };
 
@@ -20,8 +32,9 @@ function FeedbackForm() {
             placeholder="Write a review"
             value={ratText}
           />
-          <Button type="submit">Submit</Button>
+          <Button type="submit" isDisabled={btnDisabled}>Submit</Button>
         </div>
+          {errMsg && <div className='message'>{errMsg}</div>}
       </form>
     </Card>
   );
